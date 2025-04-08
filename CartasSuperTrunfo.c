7942e079381baf16ee5414b95a7d2b16c2c6b017
Rafael Correa <scriptsengineer@gmail.com>
2025-04-08 23:55:01 +0000

    Feito o desafio novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,75 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    // Dados da primeira cidade
+    int city_code_01 = 0;
+    char city_name_01[20];
+    int population_01 = 0;
+    float area_01 = 0;
+    float pib_01 = 0;
+    int number_of_tourist_attractions_01 = 0;
+
+    // Dados da segunda cidade
+    int city_code_02 = 0;
+    char city_name_02[20];
+    int population_02 = 0;
+    float area_02 = 0;
+    float pib_02 = 0;
+    int number_of_tourist_attractions_02 = 0;
+
+    // Leitura dos dados da primeira cidade
+    printf("---------------------------------------\n");
+    printf("= Digite os dados da primeira cidade =\n");
+    printf("Digite o código da cidade:");
+    scanf("%i", &city_code_01);
+    printf("Digite o nome da cidade:");
+    scanf("%s", city_name_01);
+    printf("Digite a população da cidade:");
+    scanf("%i", &population_01);
+    printf("Digite a area da cidade:");
+    scanf("%f", &area_01);
+    printf("Digite o pib da cidade:");
+    scanf("%f", &pib_01);
+    printf("Digite o número de pontos turisticos da cidade:");
+    scanf("%i", &number_of_tourist_attractions_01);
+
+
+    // Leitura dos dados da segunda cidade
+    printf("---------------------------------------\n");
+    printf("= Digite os dados da segunda cidade =\n");
+    printf("Digite o código da cidade:");
+    scanf("%i", &city_code_02);
+    printf("Digite o nome da cidade:");
+    scanf("%s", city_name_02);
+    printf("Digite a população da cidade:");
+    scanf("%i", &population_02);
+    printf("Digite a area da cidade:");
+    scanf("%f", &area_02);
+    printf("Digite o pib da cidade:");
+    scanf("%f", &pib_02);
+    printf("Digite o número de pontos turisticos da cidade:");
+    scanf("%i", &number_of_tourist_attractions_02);
+
+
+    // Exibindo informaçãoes da primeira cidade
+    printf("---------------------------------------\n");
+    printf("Exibindo dados da cidade código [%i]\n", city_code_01);
+    printf("\tNome: %s\n", city_name_01);
+    printf("\tPopulação: %i\n", population_01);
+    printf("\tArea: %.2f\n", area_01);
+    printf("\tPIB: %.2f\n", pib_01);
+    printf("\tNúmero de pontos turisticos: %i\n", number_of_tourist_attractions_01);
+
+    // Exibindo informaçãoes da segunda cidade
+    printf("-------------\n");
+    printf("Exibindo dados da cidade código [%i]\n", city_code_02);
+    printf("\tNome: %s\n", city_name_02);
+    printf("\tPopulação: %i\n", population_02);
+    printf("\tArea: %.2f\n", area_02);
+    printf("\tPIB: %.2f\n", pib_02);
+    printf("\tNúmero de pontos turisticos: %i\n", number_of_tourist_attractions_02);
+
 
     return 0;
 }
